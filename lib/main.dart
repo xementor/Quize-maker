a9b04c5fb42b815d3985da1d1e50b41f84225c58
xementor <ahammedzonaid@gmail.com>
2022-08-26 07:59:32 +0600

    start copying fireship app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_search_bar/flutter_search_bar.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -11,73 +8,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      navigatorObservers: [], //with flutter analytics],
+
+      routes: {
+        '/': (context) => LoginScreen(),
+        '/topic': (context) => TopicsScreen(),
+        '/profile': (context) => ProfileScreen(),
+        '/about': (context) => AboutScreen(),
+      },
+
+      // Theme:
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  late SearchBar searchBar;
-
-  AppBar buildAppBar(BuildContext contxt) {
-    return new AppBar(
-      title: const Text('My Home page'),
-      actions: [searchBar.getSearchAction(context)],
-    );
-  }
-
-  _MyHomePageState() {
-    searchBar = new SearchBar(
-      setState: setState,
-      buildDefaultAppBar: buildAppBar,
-      onSubmitted: print,
-      inBar: false,
-    );
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: searchBar.build(context),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        fontFamily: 'Nunito',
+        bottomAppBarTheme: BottomAppBarTheme(
+          color: Colors.black87,
+        ),
+        brightness: Brightness.dark,
+        textTheme: TextTheme(
+          bodyText1: TextStyle(
+            fontSize: 18,
+          ),
+          bodyText2: TextStyle(fontSize: 16),
+          button: TextStyle(letterSpacing: 1.5, fontWeight: FontWeight.bold),
+          headline1: TextStyle(fontWeight: FontWeight.bold),
+          subtitle1: TextStyle(color: Colors.grey),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
